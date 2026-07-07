d9cb20a0fe35d6d75f3d64eea426031c5e4ac53e
Kumaresan <73640429+kumaresang12@users.noreply.github.com>
2026-07-07 15:55:01 -0700

    Add Terraform configuration for Dynatrace provider

---
+++ terraform/nonprod/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    dynatrace = {
+      source  = "dynatrace-oss/dynatrace"
+      version = "~> 1.98"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+  }
+}
+
+provider "dynatrace" {}
+
+variable "backup_dir" {
+  default = "dynatrace_backup"
+}
+
+resource "null_resource" "dynatrace_config_export" {
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+set -e
+
+mkdir -p ${var.backup_dir}
+
+export DYNATRACE_TARGET_FOLDER="${var.backup_dir}/$(date +%F)"
+
+PROVIDER_BIN=$(find .terraform/providers/registry.terraform.io/dynatrace-oss/dynatrace -type f -name "terraform-provider-dynatrace*" | head -n 1)
+
+"$PROVIDER_BIN" -export -ref -id -flat
+EOT
+  }
+}
+
+output "backup_location" {
+  value = var.backup_dir
+}
